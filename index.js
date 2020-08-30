9140ddc143beb3b004ebd2de4e52e7c20b20af4b
Julian Ayoub <julianayoub1@outlook.com>
2020-08-30 12:00:07 +1000

    Test

---
+++ index.js
@@ -0,0 +1,12 @@
+//doctor logs in
+//has a list of patients assigned
+//clicks on patient
+//pressess "prescriptions"
+//has list of drugs available to prescribe / text box to enter in
+//submits prescription
+//user is assigned prescribed drug
+//user can print out prescription
+//this prescription stays tied to user account
+
+
+
